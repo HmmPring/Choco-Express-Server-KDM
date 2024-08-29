1741a0c42a45ab80dc38ae5a6c66b3db472583f0
JeongHeumChoi <andyc707@gmail.com>
2024-08-30 00:46:43 +0900

    Feat: healthCheck API 추가

--- src/main/java/dgu/choco_express/constant/Constants.java
+++ src/main/java/dgu/choco_express/constant/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
     public static String ACCESS_COOKIE_NAME = "access_token";
     public static String REFRESH_COOKIE_NAME = "refresh_token";
     public static List<String> NO_NEED_AUTH = List.of(
+            "/api/health-check",
             "/api/auth/sign-up",
             "/api/auth/sign-in",
             "/oauth2/authorization/kakao"
