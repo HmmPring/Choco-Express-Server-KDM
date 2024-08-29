f58a2e9c68c3e1e700ceb0775fcf7a9e8735d27c
JeongHeumChoi <andyc707@gmail.com>
2024-08-29 21:46:02 +0900

    Feat: 스프링 시큐리티, oauth2 및 기본 설정 완료

---
+++ src/main/java/dgu/choco_express/interceptor/post/ResponseInterceptor.java
@@ -0,0 +1,34 @@
+package dgu.choco_express.interceptor.post;
+
+import dgu.choco_express.dto.common.ResponseDto;
+import dgu.choco_express.exception.ErrorResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class ResponseInterceptor implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return !(returnType.getParameterType() == ResponseDto.class)
+                && MappingJackson2HttpMessageConverter.class.isAssignableFrom(converterType);
+    }
+
+    @Override
+    public Object beforeBodyWrite(
+            Object body,
+            MethodParameter returnType,
+            MediaType selectedContentType,
+            Class selectedConverterType,
+            ServerHttpRequest request,
+            ServerHttpResponse response
+    ) {
+        if(body instanceof ErrorResponse)
+            return ResponseDto.fail((ErrorResponse)body);
+        return ResponseDto.success(body);
+    }
+}
